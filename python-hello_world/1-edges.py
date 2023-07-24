f866b961f466cd1f56a26ed9ff7796179ddd3fc7
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2023-07-24 04:21:22 +0100

    Python edges for Holberton

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print("First 3 letters: {}".format("Hol"))
 print("Last 2 letters: {}".format("on"))
 print ("Middle word: {}".format("olberto"))
