5fe336e39ebfb7e96346051466f18dd66575f37c
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2023-07-27 08:28:37 +0100

    python edges for holberton school

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,6 +3,6 @@ word = "Holberton" "School"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("First 3 letters: {} {}".format("Sch", "Hol"))
-print("Last 2 letters: {} {}".format("ol", "on"))
-print ("Middle word: {} {}".format("choo", "olberto"))
+print("First 3 letters: {}".format("Hol"))
+print("Last 2 letters: {}".format("on"))
+print ("Middle word: {}".format("olberto"))
