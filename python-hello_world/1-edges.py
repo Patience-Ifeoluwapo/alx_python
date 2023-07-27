4ff86e09c8a4d8b18b4f9f91d612efe271e4f4e5
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2023-07-27 08:47:54 +0100

    python edges

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton" "School"
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
+word = "Holberton"
+first_3_letters = word[:3]
+last_2_letters = word[-2:]
 middle_word = word[1:-1]
-print("First 3 letters: {}".format("Hol"))
-print("Last 2 letters: {}".format("on"))
-print ("Middle word: {}".format("olberto"))
+print("First 3 characters are {}".format("first_three_letters"))
+print("Last 2 characters are {}".format("last_two_letters"))
+print ("Middle word is {}".format("middle_word"))
