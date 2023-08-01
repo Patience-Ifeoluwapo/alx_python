5b2ddeb9882cb911127d42c3f4763d143714d39a
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2023-08-01 04:31:25 +0100

    password validation function

---
+++ python-functions/6-password.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+def validate_password(password):
+    # Check the password length
+    if len(password) < 8:
+        return False
+    
+    #Check for at least one uppercase, one lowercase, and one digit
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_uppercase = True
+        elif char.islower():
+            has_lowercase = True
+        elif char.isdigit():
+            has_digit = True
+
+    if not (has_uppercase and has_lowercase and has_digit):
+        return False
+
+    # Check for spaces
+    if ' ' in password:
+        return False
+
+    return True
