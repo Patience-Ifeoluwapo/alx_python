f9c22a1f716de3678d8efcc7b748376774666736
Patience-Ifeoluwapo <ifeoluwapo.babatunde@gmail.com>
2023-08-03 07:26:27 +0100

    creating square class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""My Custom Square Module"""
 
 class Square:
+    """My Square Class Definition"""
     def __init__(self, size):
+        """Instantiate a new square object"""
         self.__size = size
